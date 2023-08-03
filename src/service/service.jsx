8f4c1397f141ba6ae9a85e2d699c8bd6b8fa8f69
Goq-96 <abrahamyan199605@inbox.ru>
2023-08-03 06:55:17 +0400

    crate achtivity and project sections

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -1,11 +1,46 @@
-import ServiceBox from '../components/serviceBox/serviceBox';
 import './service.scss'
+import { BiCodeAlt } from 'react-icons/bi';
+import { AiOutlineAim, AiOutlineAntDesign, AiOutlineDesktop } from "react-icons/ai";
 
 
 const Service = () => {
+
+    const serviceList = [
+        {
+            logo: <AiOutlineAntDesign />,
+            title: "Graphics Design",
+            text: "Develop the Visual Identity of Your Business"
+        },
+        {
+            logo: <AiOutlineDesktop />,
+            title: "Web Design",
+            text: "Connect With Your Users, Not Just Your Business."
+        },
+        {
+            logo: <BiCodeAlt />,
+            title: "Development",
+            text: "We Develop the Visual Identity of Your Business."
+        },
+        {
+            logo: <AiOutlineAim />,
+            title: "Seo Friendly",
+            text: "Taking your site at the top of Google's ranking."
+        }
+    ]
+
     return (
         <div className="serviceContainer">
-            <ServiceBox />
+            {
+                serviceList.map(({ logo, title, text }, index) => (
+                    <div className="serviceBox" key={index}>
+                        <div className='logo'>
+                            {logo}
+                        </div>
+                        <h4>{title}</h4>
+                        <p>{text}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
