04edecfb0c836984b0fe857822e1516707a48a4d
abrahamyan77 <abrahamyan051996@mail.com>
2023-12-02 08:20:44 +0400

    change activiti section component

--- src/portfolio/portfolio.jsx
+++ src/portfolio/portfolio.jsx
@@ -2,36 +2,42 @@ import { useInView } from "react-intersection-observer";
 import Container from "../components/container/container";
 import "./portfolio.scss"
 import { useEffect, useRef } from "react";
+import boximg from "../images/box.png"
+import { NavLink } from "react-router-dom";
 
 
 const PortfolioItems = (props) => {
 
- 
-
     const Items = [
         {
-            img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/11.jpg",
+            id: 1,
+            img: boximg,
             title1: "3D Graphics",
             title2: "Templates",
         },
         {
+            id: 2,
             img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/22.jpg",
             title1: "Web Design"
         },
         {
+            id: 3,
             img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/33.jpg",
             title1: "Web Design"
         },
         {
+            id: 4,
             img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/44.jpg",
             title1: "Development"
         },
         {
+            id: 5,
             img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/55.jpg",
             title1: "Templates",
             title2: "Web Design",
         },
         {
+            id: 6,
             img: "https://maraviyainfotech.com/projects/masterly/demo-2/assets/img/portfolio/66.jpg",
             title1: "Development",
             title2: "3D Graphics",
@@ -40,19 +46,25 @@ const PortfolioItems = (props) => {
     ]
     return (
 
-        Items.map(({ img, title1, title2 }, index) => (
+        Items.map(({ img, title1, title2, id }, index) => (
             <div className="ItemWrapper ItemGrid" key={index}  >
-                <div className="ItemsInner" >
-                    <div className="ItemsImg">
-                        <img src={img} alt="img" />
-                        <div className="overlay">
-                            <h3>
-                                <span>{title1}</span>
-                                {title2 && <span>{title2}</span>}
-                            </h3>
+                <NavLink to={
+                    id ===1 &&'https://abrahamyan777.github.io/' ||
+                    id ===2 && "https://google.com"
+                            }
+             target="blank">
+                    <div className="ItemsInner" >
+                        <div className="ItemsImg">
+                            <img src={img} alt="img" />
+                            <div className="overlay">
+                                <h3>
+                                    <span>{title1}</span>
+                                    {title2 && <span>{title2}</span>}
+                                </h3>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </NavLink>
             </div>
         ))
 
@@ -69,7 +81,7 @@ const Portfolio = (props) => {
 
     useEffect(() => {
         if (ElIsVisible) {
-           itemsRef.current.classList.add("animationPort")
+            itemsRef.current.classList.add("animationPort")
         }
 
     }, [ElIsVisible])
